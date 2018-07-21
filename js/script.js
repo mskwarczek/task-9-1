b7666e6d22c36c26fb34863b67a9fe2e0f022a62
Maciej Skwarczek <m.skwarczek@gmail.com>
2018-07-21 11:27:46 +0200

    Add js and styles

---
+++ js/script.js
@@ -0,0 +1,43 @@
+'use strict'
+
+var data = [
+	{
+		id: 'box1',
+		title: 'First box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'special-header', 'important']
+	},
+	{
+		id: 'box2',
+		title: 'Second box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['special-header', 'important']
+	},
+	{
+		id: 'box3',
+		title: 'Third box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted', 'important']
+	},
+	{
+		id: 'box4',
+		title: 'Fourth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: ['highlighted']
+	},
+	{
+		id: 'box5',
+		title: 'Fifth box',
+		content: '<p>Lorem ipsum dolor sit amet!</p>',
+		categories: []
+	},
+];
+
+
+for (var i = 0; i < 5; i++) {
+	var box = document.createElement("div");
+	box.innerHTML = "<h1>" + data[i].title + "</h1>" + data[i].content;
+	box.setAttribute("id", data[i].id);
+	box.setAttribute("class", data[i].categories.join(" "));
+	document.getElementById("main").appendChild(box);
+}
